559d6d5784da84ba41bc2942b46f5b407943658d
Shivam Mishra <82814848+AnthonyHowardStark@users.noreply.github.com>
2022-09-26 23:59:34 +0530

    Add files via upload

---
+++ Code 3 Rotator.cs
@@ -0,0 +1,12 @@
+Code 3 Rotator
+
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    void Update () 
+    {
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+}
